6106f8e6ad4d5108a1ca95fc7691f9b28cfc5331
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2016-02-16 13:33:16 -0800

    Uses the  service

--- test/main.spec.js
+++ test/main.spec.js
@@ -4,16 +4,23 @@ var expect = chai.expect;
 describe("The Address Book App", function () {
   
   describe('Contact Service', function (){
-
-    it("has a array of Contacts", function () {
+    beforeEach(function () {
       module('AddressBook')
       inject(function ($injector) {
-        contactService = $injector.get('contactService');  
+        contactService = $injector.get('contactService');
+        $httpBackend = $injector.get('$httpBackend');
       });
+    });
 
+    it("has a array of Contacts", function () {
       expect(contactService.contacts).to.be.an('array');
+    });
 
-    })
+    it("calls the /contacts route", function () {
+      $httpBackend.expectGET('http://localhost:9001/contacts')
+        .respond(200, []);
+      $httpBackend.flush();
+    });
 
   });
 
